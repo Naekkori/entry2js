57cf7bb4d183a1a93d5e81f1b8df4ef82babce77
nkl <spinpo12@naver.com>
2025-07-04 20:42:53 +0900

    test

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 //트랜스파일 테스트
 import fs from "fs";
-import path from "node:path";
 import {test_ast} from "./e2jast.js";
-var projectJson = JSON.parse(fs.readFileSync(path.join("C:\\Users\\Administrator\\Documents\\entry2js-extract-1751602207820", "project.json"), 'utf8'));
+
+// 커맨드 라인에서 파일 경로를 인자로 받습니다.
+const projectJsonPath = process.argv[2];
+
+if (!projectJsonPath) {
+    console.error("Error: Please provide the path to project.json as an argument.");
+    console.log("Usage: node test.js <path/to/project.json>");
+    process.exit(1);
+}
+
+const projectJson = JSON.parse(fs.readFileSync(projectJsonPath, 'utf8'));
 if (Array.isArray(projectJson.objects)) {
     for (const obj of projectJson.objects){
         if (obj&&obj.script){
